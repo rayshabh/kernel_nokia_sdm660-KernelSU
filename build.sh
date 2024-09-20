29e364b39e567cc055106fb4982f5398cb0dbebc
rayshabh <khargareeshav@gmail.com>
2024-09-20 22:48:05 +0530

    build.sh: replaced existing buildscript

--- build.sh
+++ build.sh
@@ -4,13 +4,6 @@ KERNEL_NAME="RedCherry-4.4.302-NokiaSDM660"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 FINAL_ZIP=$KERNEL_NAME-$DATE.zip
 
-# Clone the repos
-clonning(){
-    git clone --depth=1 https://github.com/Xiaomi-SD720G-Devices/AOSP-clang.git toolchains/clang
-    git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git toolchains/arm64-gcc
-    git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git toolchains/arm-gcc
-    git clone --depth=1 https://github.com/userariii/AnyKernel3.git -b NokiaSDM660 toolchains/AnyKernel3
-}
 # Determine the number of threads for compilation
 determine_threads() {
     echo -e "Determining no. of threads...\n"
@@ -40,7 +33,7 @@ configure_kernel() {
 # Build the kernel
 build_kernel() {
     echo -e "Building the kernel...\n"
-    PATH="$(pwd)/toolchains/clang/bin:$(pwd)/toolchains/arm64-gcc/bin:$(pwd)/toolchains/arm-gcc/bin:${PATH}" \
+    PATH="/home/rayshabh/.android/kernels/toolchains/clang/bin:/home/rayshabh/.android/kernels/toolchains/arm64-gcc/bin:/home/rayshabh/.android/kernels/toolchains/arm-gcc/bin:${PATH}" \
     make -j $THREADS O=out \
         ARCH=arm64 \
         CC=clang \
@@ -53,9 +46,9 @@ build_kernel() {
 # Package the kernel into a zip file
 package_kernel() {
     echo -e "Packing the kernel into a zip file using AnyKernel3...\n"
-    local kernel_image="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
-    local anykernel_dir="$(pwd)/toolchains/AnyKernel3"
-    local kernel_builds_dir="$(pwd)/KERNEL_BUILDS"
+    local kernel_image="$HOME/.android/kernels/kernel_nokia_sdm660-KernelSU/out/arch/arm64/boot/Image.gz-dtb"
+    local anykernel_dir="$HOME/.android/kernels/toolchains/AnyKernel3"
+    local kernel_builds_dir="$HOME/.android/kernels/KERNEL_BUILDS"
 
     cp $kernel_image $anykernel_dir/
     cd && mkdir -p $kernel_builds_dir
@@ -63,13 +56,11 @@ package_kernel() {
     zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
     mv $anykernel_dir/UPDATE-AnyKernel2.zip $kernel_builds_dir/$FINAL_ZIP
     rm -rf $anykernel_dir/Image.gz-dtb
-    cd ../..
     echo -e "Done!\n"
 }
 
 # Main function
 main() {
-    clonning
     determine_threads
     clean_build_environment
     configure_kernel
